eaa3a4cb72c538f6341588871383e7a049f34fdd
F.D.Castel <fdcastel@gmail.com>
2022-12-04 23:21:43 -0300

    Adds CheckInMonitor.

---
+++ Deucalion.Core/Monitors/CheckInMonitor.cs
@@ -0,0 +1,37 @@
+﻿using Deucalion.Monitors.Options;
+
+namespace Deucalion.Monitors
+{
+    public class CheckInMonitor : IMonitor<MonitorOptions>
+    {
+        private readonly ManualResetEvent _checkInEvent = new(false);
+        private Timer? _resetTimer = null;
+
+        public required MonitorOptions Options { get; init; }
+
+        public Task<bool> IsUpAsync()
+        {
+            bool result = _checkInEvent.WaitOne(TimeSpan.Zero);
+            return Task.FromResult(result);
+        }
+
+        public void CheckIn()
+        {
+            _ = _checkInEvent.Set();
+
+            if (_resetTimer is null)
+            {
+                _resetTimer = new(Reset, null, Options.IntervalWhenUpOrDefault, Timeout.InfiniteTimeSpan);
+            }
+            else
+            {
+                _ = _resetTimer.Change(Options.IntervalWhenUpOrDefault, Timeout.InfiniteTimeSpan);
+            }
+        }
+
+        private void Reset(object? state)
+        {
+            _ = _checkInEvent.Reset();
+        }
+    }
+}
---
+++ Deucalion.Tests/Monitors/CheckInMonitorTests.cs
@@ -0,0 +1,39 @@
+using Deucalion.Monitors;
+using Xunit;
+
+namespace Deucalion.Tests.Monitors
+{
+    public class CheckInMonitorTests
+    {
+        [Fact]
+        public async Task CheckInMonitor_ReturnsFalse_WhenInitialized()
+        {
+            CheckInMonitor checkInMonitor = new() { Options = new() };
+            bool result = await checkInMonitor.IsUpAsync();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task CheckInMonitor_ReturnsTrue_WhenCheckedIn()
+        {
+            CheckInMonitor checkInMonitor = new() { Options = new() };
+            checkInMonitor.CheckIn();
+            bool result = await checkInMonitor.IsUpAsync();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task CheckInMonitor_ReturnsFalse_WhenNotCheckedIn()
+        {
+            CheckInMonitor checkInMonitor = new() { Options = new() { IntervalWhenUp = TimeSpan.FromMilliseconds(500) } };
+            checkInMonitor.CheckIn();
+            bool result = await checkInMonitor.IsUpAsync();
+            Assert.True(result);
+
+            await Task.Delay(checkInMonitor.Options.IntervalWhenUpOrDefault * 2);
+
+            result = await checkInMonitor.IsUpAsync();
+            Assert.False(result);
+        }
+    }
+}
