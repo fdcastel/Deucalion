2ad4c0d357223214fc2f5a3a78196bfd504c9165
F.D.Castel <fdcastel@gmail.com>
2023-05-19 23:07:13 -0300

    Convert MonitorState to enum.

--- Deucalion.Core/Monitors/MonitorState.cs
+++ Deucalion.Core/Monitors/MonitorState.cs
@@ -1,27 +1,8 @@
 ﻿namespace Deucalion.Monitors;
 
-public class MonitorState
+public enum MonitorState
 {
-    protected MonitorState() { }
-
-    public static MonitorState Unknown { get; } = new();
-    public static MonitorState Down { get; } = new();
-    public static MonitorState Up { get; } = new();
-
-    public static MonitorState FromBool(bool value) => value ? Up : Down;
-    public static bool? ToBool(MonitorState value)
-    {
-        if (value == null || value == Unknown)
-            return null;
-
-        return value == Up;
-    }
-
-    public override string ToString() =>
-        ToBool(this) switch
-        {
-            true => "Up",
-            false => "Down",
-            _ => "Unknown"
-        };
+    Unknown = -1,
+    Down = 0,
+    Up = 1
 }
