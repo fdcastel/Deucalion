db6a54c2f6ab296be306ad5645e916ad7b50cab5
F.D.Castel <fdcastel@gmail.com>
2023-08-20 18:42:38 -0300

    Refactor Program.cs.

--- src/cs/Deucalion.Service/Program.cs
+++ src/cs/Deucalion.Service/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Deucalion.Api;
-using Microsoft.AspNetCore.Http.Features;
+using Deucalion.Service;
 
+// During development use "wwwroot" from publish folder.
 var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
 var webRootPath = isDevelopment ? "../../../publish/wwwroot" : "./wwwroot";
 
@@ -12,47 +13,8 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
 
 builder.Services.AddWindowsService();
 
-var app = builder.ConfigureApplicationBuilder()
-    .Build();
-
-app.UseStaticFiles();
-app.ConfigureApplication();
-
-app.Use(async (context, next) =>
-{
-    // Serve "index.html" in "/" replacing "import_meta_env_placeholder\".
-    if (context.Request.Path == "/")
-    {
-        // Workaround for "Synchronous operations are disallowed" error in Linux. (!?)
-        //   -- https://stackoverflow.com/a/67632199/33244
-        var syncIOFeature = context.Features.Get<IHttpBodyControlFeature>();
-        if (syncIOFeature != null)
-        {
-            syncIOFeature.AllowSynchronousIO = true;
-        }
-
-        var pageTitle = Environment.GetEnvironmentVariable("DEUCALION_PAGE_TITLE");
-        var apiUrl = Environment.GetEnvironmentVariable("DEUCALION_API_URL");
-
-        var importMetaEnvJson = "{\"DEUCALION_PAGE_TITLE\":\"" + pageTitle + "\",\"DEUCALION_API_URL\":\"" + apiUrl + "\"}";
-
-        var indexFile = app.Environment.WebRootFileProvider.GetFileInfo("/index.html").PhysicalPath;
-        if (indexFile is not null)
-        {
-            var indexContent = await File.ReadAllTextAsync(indexFile);
-
-            var newContent = indexContent.Replace(
-                "\"import_meta_env_placeholder\"",
-                importMetaEnvJson
-            );
-
-            using var bodyWriter = new StreamWriter(context.Response.Body);
-            await bodyWriter.WriteAsync(newContent);
-            return;
-        }
-    }
-
-    await next();
-});
-
-app.Run();
+builder.ConfigureApplicationBuilder()
+    .Build()
+    .ConfigureApplication()
+    .UseDeucalionSpa()
+    .Run();
---
+++ src/cs/Deucalion.Service/WebApplicationExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+
+namespace Deucalion.Service;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication UseDeucalionSpa(this WebApplication app)
+    {
+        app.UseStaticFiles();
+
+        app.Use(async (context, next) =>
+        {
+            // Serve "index.html" in "/" replacing "import_meta_env_placeholder\".
+            if (context.Request.Path == "/")
+            {
+                // Workaround for "Synchronous operations are disallowed" error in Linux. (!?)
+                //   -- https://stackoverflow.com/a/67632199/33244
+                var syncIOFeature = context.Features.Get<IHttpBodyControlFeature>();
+                if (syncIOFeature != null)
+                {
+                    syncIOFeature.AllowSynchronousIO = true;
+                }
+
+                var pageTitle = Environment.GetEnvironmentVariable("DEUCALION_PAGE_TITLE");
+                var apiUrl = Environment.GetEnvironmentVariable("DEUCALION_API_URL");
+
+                var importMetaEnvJson = "{\"DEUCALION_PAGE_TITLE\":\"" + pageTitle + "\",\"DEUCALION_API_URL\":\"" + apiUrl + "\"}";
+
+                var indexFile = app.Environment.WebRootFileProvider.GetFileInfo("/index.html").PhysicalPath;
+                if (indexFile is not null)
+                {
+                    var indexContent = await File.ReadAllTextAsync(indexFile);
+
+                    var newContent = indexContent.Replace(
+                        "\"import_meta_env_placeholder\"",
+                        importMetaEnvJson
+                    );
+
+                    using var bodyWriter = new StreamWriter(context.Response.Body);
+                    await bodyWriter.WriteAsync(newContent);
+                    return;
+                }
+            }
+
+            await next();
+        });
+
+        return app;
+    }
+}
