b0a2f798331e32d5110bd8f6afde9aee5d0f6024
F.D.Castel <fdcastel@gmail.com>
2024-05-27 03:34:14 -0300

    Improve build scripts.

--- Deucalion.build.ps1
+++ Deucalion.build.ps1
@@ -6,35 +6,47 @@
 $configuration = 'Release'
 $publishFolder = './publish'
 
-task Clean {
-    Remove-BuildItem */dist, $publishFolder,
-        .\src\cs\*\bin, 
-        .\src\cs\*\obj, 
-        .\src\ts\deucalion-ui\bin,
-        .\src\ts\deucalion-ui\obj,
-        .\src\ts\deucalion-ui\dist
-
+# synopsis: Remove build artifacts.
+task Clear {
+    remove */dist,
+        $publishFolder,
+        ./src/cs/*/bin,
+        ./src/cs/*/obj,
+        ./src/ts/deucalion-ui/bin,
+        ./src/ts/deucalion-ui/obj,
+        ./src/ts/deucalion-ui/dist
 }
 
-task Clean-Full Clean, {
-    Remove-BuildItem .\src\ts\deucalion-ui\node_modules
+# synopsis: Remove build artifacts and node_modules.
+task Clear-Npm Clear, {
+    remove ./src/ts/deucalion-ui/node_modules
 }
 
-task Build {
+# synopsis: Clear and build projects.
+task Build Clear, {
     exec { dotnet publish './src/cs/Deucalion.Service/Deucalion.Service.csproj' -c $configuration -o $publishFolder -p:DebugType=None --self-contained }
 
     exec { npm --prefix './src/ts/deucalion-ui' ci }
-
     exec { npm --prefix './src/ts/deucalion-ui' run build -- --outDir "../../../$publishFolder/wwwroot" }
 }
 
-task Test Build, {
-    # Build and run a production service
+# synopsis: Start a development environment.
+task Dev {
+    Start-Process powershell { npm --prefix './src/ts/deucalion-ui' run dev }
+    exec { dotnet watch --project './src/cs/Deucalion.Api/Deucalion.Api.csproj' }
+}
 
+# synopsis: Run a production service.
+task Prod {
     Set-Location './publish'
     Start-Process 'http://localhost:5000'
 
-    exec { ./Deucalion.Service.exe /Deucalion:ConfigurationFile=..\deucalion-sample.yaml }
+    exec { ./Deucalion.Service.exe /Deucalion:ConfigurationFile=../deucalion-sample.yaml }
 }
 
-task . Clean, Build
+# synopsis: Run test suite.
+task Test {
+    exec { dotnet test }
+}
+
+task . Build
