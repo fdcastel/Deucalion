ebe1999a96a6d49a401a833dd5d352fa2aa1be40
F.D.Castel <fdcastel@gmail.com>
2025-05-20 06:16:42 -0300

    [wip] Adds HeroUI.

--- src/ts/deucalion-ui/src/components/app.tsx
+++ src/ts/deucalion-ui/src/components/app.tsx
@@ -8,6 +8,7 @@ import { logger } from "../services";
 import { preloadConfiguration, useConfiguration } from "../contexts/ConfigurationContext";
 import { preloadMonitors, useMonitors } from "../contexts/MonitorsContext";
 import { useMonitorHubContext } from "../contexts/MonitorHubContext";
+import { Button } from "@heroui/react";
 
 preloadConfiguration();
 preloadMonitors();
@@ -22,17 +23,35 @@ export const App = () => {
   const { isConnected, isConnecting, connectionError } = useMonitorHubContext();
 
   return (
-    <Container maxWidth="container.xl" padding="0">
-      <Flex direction="column" padding="2">
-        <Overview
-          title={configurationData?.pageTitle ?? "Deucalion Status"}
-          monitors={monitorsData ?? EMPTY_MONITORS}
-          isConnected={isConnected}
-          isConnecting={isConnecting}
-          connectionError={connectionError}
-        />
-        <MonitorList groupedMonitors={groupedMonitorsData} usingImages={usingImages} />
-      </Flex>
-    </Container>
+    <>
+      <Container maxWidth="container.xl" padding="0">
+        <Flex direction="column" padding="2">
+          <Overview
+            title={configurationData?.pageTitle ?? "Deucalion Status"}
+            monitors={monitorsData ?? EMPTY_MONITORS}
+            isConnected={isConnected}
+            isConnecting={isConnecting}
+            connectionError={connectionError}
+          />
+          <MonitorList groupedMonitors={groupedMonitorsData} usingImages={usingImages} />
+        </Flex>
+      </Container>
+
+      <div className="relative flex flex-col h-screen">
+        <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+
+          <h1>Vite + React</h1>
+          <div className="card">
+            <Button color="primary">
+              count is X
+            </Button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+
+        </main>
+      </div>
+    </>
   );
 };
--- src/ts/deucalion-ui/src/main.tsx
+++ src/ts/deucalion-ui/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import {HeroUIProvider} from '@heroui/react'
+
 import { ChakraEnvironment } from "./chakra-environment";
 import { App } from "./components/app";
 
@@ -8,20 +10,24 @@ import { ConfigurationProvider } from "./contexts/ConfigurationContext";
 import { MonitorsProvider } from "./contexts/MonitorsContext";
 import { MonitorHubProvider } from "./contexts/MonitorHubContext";
 
+import "./index.css";
+
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 
 ReactDOM.createRoot(container).render(
   <React.StrictMode>
-    <ChakraEnvironment>
-      <ConfigurationProvider>
-        <MonitorsProvider>
-          <MonitorHubProvider>
-            <App />
-          </MonitorHubProvider>
-        </MonitorsProvider>
-      </ConfigurationProvider>
-    </ChakraEnvironment>
+    <HeroUIProvider>
+      <ChakraEnvironment>
+        <ConfigurationProvider>
+          <MonitorsProvider>
+            <MonitorHubProvider>
+              <App />
+            </MonitorHubProvider>
+          </MonitorsProvider>
+        </ConfigurationProvider>
+      </ChakraEnvironment>
+    </HeroUIProvider>    
   </React.StrictMode>
 );
 
