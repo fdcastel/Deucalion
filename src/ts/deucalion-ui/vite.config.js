39e884ec77c625208cb30e79b0a3f0055d03fefd
F.D.Castel <fdcastel@gmail.com>
2025-05-21 07:51:49 -0300

    Removes pure annotations from SignalR.

--- src/ts/deucalion-ui/vite.config.js
+++ src/ts/deucalion-ui/vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         chunkSizeWarningLimit: 1000,
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+        {
+            // Removes pure annotations from SignalR -- https://github.com/dotnet/aspnetcore/issues/55286#issuecomment-2557288741
+            name: 'remove-pure-annotations',
+            enforce: 'pre',
+            transform: function (code, id) {
+                return id.indexOf('node_modules/@microsoft/signalr') !== -1
+                    ? code.replace(/\/\*#__PURE__\*\//g, '')
+                    : null;
+            }
+        }
+    ],
     server: {
         proxy: {
             "/api/monitors/hub": {
