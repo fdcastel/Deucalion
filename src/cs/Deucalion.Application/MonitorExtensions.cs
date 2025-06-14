e1c94ee2e55eda0d910b60d883db24898584e58c
F.D.Castel <fdcastel@gmail.com>
2025-06-14 20:05:36 -0300

    Move PullMonitor RunAsync to MonitorExtensions.

--- src/cs/Deucalion.Application/MonitorExtensions.cs
+++ src/cs/Deucalion.Application/MonitorExtensions.cs
@@ -8,19 +8,10 @@ public static class MonitorExtensions
 {
     public static async Task RunAllAsync(this IEnumerable<PullMonitor> monitors, ChannelWriter<IMonitorEvent> writer, CancellationToken stopToken)
     {
-        var monitorTasks = new List<Task>();
-
-        foreach (var monitor in monitors)
-        {
-            if (monitor is PullMonitor pullMonitor)
-            {
-                monitorTasks.Add(pullMonitor.RunMonitorLoopAsync(writer, stopToken));
-            }
-        }
-
         try
         {
-            await Task.WhenAll(monitorTasks);
+            var allTasks = monitors.Select(monitor => monitor.RunAsync(writer, stopToken)).ToList();
+            await Task.WhenAll(allTasks);
         }
         catch (OperationCanceledException)
         {
@@ -30,5 +21,82 @@ public static class MonitorExtensions
         {
             writer.TryComplete();
         }
+    }
+
+    public static async Task RunAsync(this PullMonitor monitor, ChannelWriter<IMonitorEvent> writer, CancellationToken stopToken)
+    {
+        var lastKnownState = MonitorState.Unknown;
+        var consecutiveFailCount = 0;
+        do
+        {
+            if (stopToken.IsCancellationRequested) break;
+
+            var queryStartTime = DateTimeOffset.UtcNow;
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var response = await monitor.QueryAsync(stopToken);
+
+            if (response.ResponseTime is null)
+            {
+                response = response with { ResponseTime = stopwatch.Elapsed };
+            }
+
+            var name = monitor.Name;
+            var initialState = response?.State ?? MonitorState.Down;
+            var effectiveState = initialState;
+
+            if (initialState == MonitorState.Up)
+            {
+                consecutiveFailCount = 0;
+            }
+            else if (initialState == MonitorState.Down || initialState == MonitorState.Warn)
+            {
+                consecutiveFailCount++;
+                if (monitor.IgnoreFailCount > 0 && consecutiveFailCount < monitor.IgnoreFailCount)
+                {
+                    effectiveState = MonitorState.Degraded;
+                }
+            }
+
+            if (monitor.UpsideDown)
+            {
+                if (effectiveState == MonitorState.Up)
+                {
+                    effectiveState = MonitorState.Down;
+                }
+                else if (effectiveState == MonitorState.Down)
+                {
+                    effectiveState = MonitorState.Up;
+                }
+            }
+
+            var effectiveResponse = response is null ? null : response with { State = effectiveState };
+            writer.TryWrite(new MonitorChecked(name, queryStartTime, effectiveResponse));
+
+            var actualStateHasChanged = lastKnownState != effectiveState && lastKnownState == MonitorState.Unknown ||
+                                        lastKnownState != MonitorState.Unknown && lastKnownState != effectiveState;
+
+            if (actualStateHasChanged)
+            {
+                writer.TryWrite(new MonitorStateChanged(name, queryStartTime, effectiveState));
+            }
+
+            lastKnownState = effectiveState;
+
+            if (stopToken.IsCancellationRequested) break;
+
+            TimeSpan delayInterval = (lastKnownState == MonitorState.Up || lastKnownState == MonitorState.Unknown)
+                ? monitor.IntervalWhenUp
+                : monitor.IntervalWhenDown;
+
+            try
+            {
+                await Task.Delay(delayInterval, stopToken);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+        while (!stopToken.IsCancellationRequested);
     }
 }
--- src/cs/Deucalion.Core/Monitors/PullMonitor.cs
+++ src/cs/Deucalion.Core/Monitors/PullMonitor.cs
@@ -1,7 +1,4 @@
-﻿using System.Threading.Channels;
-using Deucalion.Events;
-
-namespace Deucalion.Monitors;
+﻿namespace Deucalion.Monitors;
 
 public abstract class PullMonitor
 {
@@ -22,93 +19,5 @@ public abstract class PullMonitor
     public TimeSpan Timeout { get; set; } = DefaultTimeout;
     public TimeSpan WarnTimeout { get; set; } = DefaultWarnTimeout;
 
-    public MonitorState LastKnownState { get; set; } = MonitorState.Unknown;
-    public int ConsecutiveFailCount { get; set; } = 0;
-
     public abstract Task<MonitorResponse> QueryAsync(CancellationToken cancellationToken = default);
-
-    public async Task RunMonitorLoopAsync(ChannelWriter<IMonitorEvent> writer, CancellationToken stopToken)
-    {
-        do
-        {
-            if (stopToken.IsCancellationRequested) break;
-
-            var queryStartTime = DateTimeOffset.UtcNow;
-            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-            var response = await QueryAsync(stopToken);
-
-            if (response.ResponseTime is null)
-            {
-                response = response with { ResponseTime = stopwatch.Elapsed };
-            }
-
-            // --- Begin merged UpdateMonitorState logic ---
-            var name = Name;
-            var initialState = response?.State ?? MonitorState.Down;
-            var effectiveState = initialState;
-
-            // --- ignoreFailCount Logic ---
-            if (initialState == MonitorState.Up)
-            {
-                ConsecutiveFailCount = 0;
-            }
-            else if (initialState == MonitorState.Down || initialState == MonitorState.Warn)
-            {
-                ConsecutiveFailCount++;
-                if (IgnoreFailCount > 0 && ConsecutiveFailCount < IgnoreFailCount)
-                {
-                    effectiveState = MonitorState.Degraded;
-                }
-                // else: effectiveState remains Down or Warn
-            }
-            // else: Unknown state doesn't change fail count or trigger Degraded
-
-            // --- upsideDown Logic ---
-            if (UpsideDown)
-            {
-                if (effectiveState == MonitorState.Up)
-                {
-                    effectiveState = MonitorState.Down;
-                }
-                else if (effectiveState == MonitorState.Down)
-                {
-                    effectiveState = MonitorState.Up;
-                }
-                // Warn and Degraded states are not flipped
-            }
-
-            // Notify response
-            var effectiveResponse = response is null ? null : response with { State = effectiveState };
-            writer.TryWrite(new MonitorChecked(name, queryStartTime, effectiveResponse)); // Use eventTime
-
-            // Send MonitorStateChanged only if the state actually changed from a known different state, or from Unknown.
-            var actualStateHasChanged = LastKnownState != effectiveState && LastKnownState == MonitorState.Unknown || // From Unknown to something else
-                                        LastKnownState != MonitorState.Unknown && LastKnownState != effectiveState;   // From a known state to a different known state
-
-            if (actualStateHasChanged)
-            {
-                // Notify change
-                writer.TryWrite(new MonitorStateChanged(name, queryStartTime, effectiveState)); // Use eventTime
-            }
-
-            LastKnownState = effectiveState;
-            // --- End merged UpdateMonitorState logic ---
-
-            if (stopToken.IsCancellationRequested) break;
-
-            TimeSpan delayInterval = (LastKnownState == MonitorState.Up || LastKnownState == MonitorState.Unknown)
-                ? IntervalWhenUp
-                : IntervalWhenDown;
-
-            try
-            {
-                await Task.Delay(delayInterval, stopToken);
-            }
-            catch (OperationCanceledException)
-            {
-                break; // Exit loop if cancellation is requested during delay
-            }
-        }
-        while (!stopToken.IsCancellationRequested);
-    }
 }
