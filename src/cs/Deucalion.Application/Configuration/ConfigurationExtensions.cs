4640526ac74d710157d9edd3da3be32d9126550b
F.D.Castel <fdcastel@gmail.com>
2026-03-26 10:59:25 -0300

    Flatten monitor configuration builder chain

--- src/cs/Deucalion.Application/Configuration/ConfigurationExtensions.cs
+++ src/cs/Deucalion.Application/Configuration/ConfigurationExtensions.cs
@@ -7,97 +7,61 @@ namespace Deucalion.Application.Configuration;
 
 public static class ConfigurationExtensions
 {
-    internal static CheckInMonitor Build(this CheckInMonitorConfiguration configuration) =>
-        new CheckInMonitor()
-            .ConfigureCheckInMonitor(configuration);
-
-    internal static DnsMonitor Build(this DnsMonitorConfiguration configuration) =>
-        new DnsMonitor()
-        {
-            Host = configuration.Host
-        }.ConfigureDnsMonitor(configuration);
-
-    internal static HttpMonitor Build(this HttpMonitorConfiguration configuration) =>
-        new HttpMonitor()
-        {
-            Url = configuration.Url
-        }.ConfigureHttpMonitor(configuration);
-
-    internal static PingMonitor Build(this PingMonitorConfiguration configuration) =>
-        new PingMonitor()
-        {
-            Host = configuration.Host
-        }.ConfigurePingMonitor(configuration);
-
-    internal static TcpMonitor Build(this TcpMonitorConfiguration configuration) =>
-        new TcpMonitor()
-        {
-            Host = configuration.Host,
-            Port = configuration.Port
-        }.ConfigureTcpMonitor(configuration);
-
-    private static CheckInMonitor ConfigureCheckInMonitor(this CheckInMonitor monitor, CheckInMonitorConfiguration configuration)
+    internal static CheckInMonitor Build(this CheckInMonitorConfiguration configuration)
     {
+        var monitor = new CheckInMonitor();
         monitor.Secret = configuration.Secret ?? monitor.Secret;
         monitor.IntervalToDown = configuration.IntervalToDown ?? monitor.IntervalToDown;
-
-        // Set polling intervals for CheckInMonitor based on IntervalToDown
         monitor.IntervalWhenUp = monitor.IntervalToDown;
         monitor.IntervalWhenDown = monitor.IntervalToDown;
-
-        monitor.ConfigureMonitor(configuration);
+        monitor.Name = configuration.Name ?? monitor.Name;
+        monitor.IgnoreFailCount = configuration.IgnoreFailCount ?? monitor.IgnoreFailCount;
+        monitor.UpsideDown = configuration.UpsideDown ?? monitor.UpsideDown;
         return monitor;
     }
 
-    private static DnsMonitor ConfigureDnsMonitor(this DnsMonitor monitor, DnsMonitorConfiguration configuration)
+    internal static DnsMonitor Build(this DnsMonitorConfiguration configuration)
     {
+        var monitor = new DnsMonitor() { Host = configuration.Host };
         monitor.RecordType = configuration.RecordType ?? monitor.RecordType;
         monitor.Resolver = configuration.Resolver ?? monitor.Resolver;
-        monitor.ConfigurePullMonitor(configuration);
+        ConfigurePullMonitor(monitor, configuration);
         return monitor;
     }
 
-    private static HttpMonitor ConfigureHttpMonitor(this HttpMonitor monitor, HttpMonitorConfiguration configuration)
+    internal static HttpMonitor Build(this HttpMonitorConfiguration configuration)
     {
-        monitor.Url = configuration.Url;
+        var monitor = new HttpMonitor() { Url = configuration.Url };
         monitor.ExpectedStatusCode = configuration.ExpectedStatusCode ?? monitor.ExpectedStatusCode;
         monitor.ExpectedResponseBodyPattern = configuration.ExpectedResponseBodyPattern ?? monitor.ExpectedResponseBodyPattern;
         monitor.IgnoreCertificateErrors = configuration.IgnoreCertificateErrors ?? monitor.IgnoreCertificateErrors;
         monitor.Method = configuration.Method ?? monitor.Method;
-        monitor.ConfigurePullMonitor(configuration);
+        ConfigurePullMonitor(monitor, configuration);
         return monitor;
     }
 
-    private static PingMonitor ConfigurePingMonitor(this PingMonitor monitor, PingMonitorConfiguration configuration)
+    internal static PingMonitor Build(this PingMonitorConfiguration configuration)
     {
-        monitor.Host = configuration.Host;
-        monitor.ConfigurePullMonitor(configuration);
+        var monitor = new PingMonitor() { Host = configuration.Host };
+        ConfigurePullMonitor(monitor, configuration);
         return monitor;
     }
 
-    private static TcpMonitor ConfigureTcpMonitor(this TcpMonitor monitor, TcpMonitorConfiguration configuration)
+    internal static TcpMonitor Build(this TcpMonitorConfiguration configuration)
     {
-        monitor.Host = configuration.Host;
-        monitor.Port = configuration.Port;
-        monitor.ConfigurePullMonitor(configuration);
+        var monitor = new TcpMonitor() { Host = configuration.Host, Port = configuration.Port };
+        ConfigurePullMonitor(monitor, configuration);
         return monitor;
     }
 
-    private static PullMonitor ConfigurePullMonitor(this PullMonitor monitor, PullMonitorConfiguration configuration)
+    private static void ConfigurePullMonitor(PullMonitor monitor, PullMonitorConfiguration configuration)
     {
         monitor.IntervalWhenUp = configuration.IntervalWhenUp ?? monitor.IntervalWhenUp;
         monitor.IntervalWhenDown = configuration.IntervalWhenDown ?? monitor.IntervalWhenDown;
         monitor.Timeout = configuration.Timeout ?? monitor.Timeout;
         monitor.WarnTimeout = configuration.WarnTimeout ?? monitor.WarnTimeout;
-        monitor.ConfigureMonitor(configuration);
-        return monitor;
-    }
-
-    private static Monitors.PullMonitor ConfigureMonitor(this Monitors.PullMonitor monitor, PullMonitorConfiguration configuration)
-    {
         monitor.Name = configuration.Name ?? monitor.Name;
         monitor.IgnoreFailCount = configuration.IgnoreFailCount ?? monitor.IgnoreFailCount;
         monitor.UpsideDown = configuration.UpsideDown ?? monitor.UpsideDown;
-        return monitor;
     }
 }
